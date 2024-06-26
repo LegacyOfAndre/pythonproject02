8c3e3f9bcec1aa60bbc71b52071d6b3559bd82aa
legacyofandre <andreluispcamilles1999@hotmail.com>
2024-06-26 00:54:03 -0300

    create display and change window menu

--- codes/Game.py
+++ codes/Game.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import pygame
+
+from codes.Menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
 
-    def Run(self, ):
-        pass
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            pass
 
