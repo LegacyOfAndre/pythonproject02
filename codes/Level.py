298e1d07aff1b0f4629bbf20749873d205d63938
legacyofandre <andreluispcamilles1999@hotmail.com>
2024-07-03 16:35:10 -0300

    change background directory and create level background

--- codes/Level.py
+++ codes/Level.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+from codes.Entity import Entity
+
 
 class Level:
     def __init__(self, window, name, menu_option):
-        self.window = None
-        self.entity_list = None
-        self.name = None
+        self.window = window
+        self.name = name
+        self.mode = menu_option # option that you choose play mode
+        self.entity_list = list[Entity] = []
 
     def run(self, ):
         pass
